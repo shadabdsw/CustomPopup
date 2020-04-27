eb02c81b3053b418ad959ae93e1bd5228cbc1dd5
Shadab Akhtar <shadab1996@gmail.com>
2020-04-27 15:49:05 +0530

    first commit

---
+++ app/src/main/java/com/thedarkshadows/custompopup/MainActivity.java
@@ -0,0 +1,39 @@
+package com.thedarkshadows.custompopup;
+
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Dialog myDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myDialog = new Dialog(this);
+
+    }
+
+    public void ShowPopup(View v) {
+        TextView textclose;
+        myDialog.setContentView(R.layout.custom_popup);
+        textclose = myDialog.findViewById(R.id.textclose);
+        textclose.setText("X");
+        textclose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myDialog.dismiss();
+            }
+        });
+        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        myDialog.show();
+    }
+}
